b6c6e1676207147c76c45ff5e7b91f90f535ca26
yerimstar <yerimhuh@naver.com>
2020-07-10 03:00:11 +0900

    20200710 study - 소수 구하기

--- 1929.py
+++ 1929.py
@@ -27,6 +27,8 @@ for i in range(M,N+1):
 import sys
 import math
 def isPrime(num):
+    if num == 1:
+        return False
     for i in range(2,int(math.sqrt(num))+1):
         if num%i == 0:
             return False
