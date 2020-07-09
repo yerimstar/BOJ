98b7c5fd49f3ce9ad20ad13386b87d639a0daf1a
yerimstar <yerimhuh@naver.com>
2020-07-10 03:12:56 +0900

    20200710 study - 베르트랑 공준

---
+++ 4948.py
@@ -0,0 +1,18 @@
+#1번 시도 - 시간 초과
+import sys
+import math
+def isPrime(num):
+    if num == 1:
+        return 0
+    for i in range(2,int(math.sqrt(num))+1):
+        if num%i == 0:
+            return 0
+    return 1
+while(1):
+    N = int(sys.stdin.readline())
+    count = 0
+    if N == 0:
+        break
+    for i in range(N+1,2*N+1):
+        count += isPrime(i)
+    print(count)
