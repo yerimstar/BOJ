411f5420a04a3ad364c1188861b63bde26acde8e
yerimstar <yerimhuh@naver.com>
2020-07-10 19:34:34 +0900

    20200710 study - 별 찍기 - 21

--- 10996.py
+++ 10996.py
@@ -1,7 +1,14 @@
 import sys
 N = int(sys.stdin.readline())
-for i in range(N):
-    if i%2 == 0:
-        print("*")
-    else:
-        print("*")
+if N == 1:
+    print("*")
+else:
+    for i in range(N):
+        if N % 2 == 0:
+            M = N//2
+            print("* "*M)
+            print(" *" *M)
+        else:
+            M = N//2+1
+            print("* "*M)
+            print(" "+"* "*(M-1))
