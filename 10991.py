4b41cbdb610357cfc07de978bf81e336beb28002
yerimstar <yerimhuh@naver.com>
2020-07-10 19:20:00 +0900

    20200710 study - 별 찍기 - 16

--- 10991.py
+++ 10991.py
@@ -1,15 +1,6 @@
 N = int(input())
-for i in range(1,N+1):
-    if i%2 == 0:
-        for k in range(1,2*i+1):
-            if k%2 == 0:
-                print(" ",end='')
-            else:
-                print("*",end='')
-    else:
-        for k in range(1,2*i+1):
-            if k%2 == 0:
-                print("*",end='')
-            else:
-                print(" ",end='')
-    print()
+if N == 1:
+    print("*")
+else:
+    for k in range(N):
+        print(" "*(N-k-1)+"* " * k+"*")
