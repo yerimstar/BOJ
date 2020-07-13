21b06c8758f845dc552b5af451668da2039f8354
yerimstar <yerimhuh@naver.com>
2020-07-14 01:05:56 +0900

    20200714 study - 블랙잭

--- 2798.py
+++ 2798.py
@@ -6,7 +6,7 @@ max = list(permutations(nums,3))
 maxs = []
 for i in range(len(max)):
     num = sum(max[i])
-    if num < M:
+    if num <= M:
         maxs.append(num)
 sort_max = sorted(maxs,reverse = True)
 print(sort_max[0])
