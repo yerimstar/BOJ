9b022698e8d09f154d89d7bdb5343af26f09b784
yerimstar <yerimhuh@naver.com>
2020-07-14 01:12:07 +0900

    20200714 study - 블랙잭

--- 2798.py
+++ 2798.py
@@ -1,11 +1,11 @@
 import sys
-from itertools import permutations
+from itertools import permutations #순열 permutations
 N,M = map(int,sys.stdin.readline().split())
-nums = list(map(int,sys.stdin.readline().split()))
-max = list(permutations(nums,3))
-maxs = []
-for i in range(len(max)):
-    num = sum(max[i])
+nums = list(map(int,sys.stdin.readline().split())) # N개의 숫자 카드
+nums_permutation = list(permutations(nums,3))# N개의 숫자 카드로 이루어진 조
+maxs = [] # 최댓값 구하기
+for i in range(len(nums_permutation)):
+    num = sum(nums_permutation[i])
     if num <= M:
         maxs.append(num)
 sort_max = sorted(maxs,reverse = True)
