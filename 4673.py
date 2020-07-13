18b811d77394a037c1ee7c5a14647d5f611394aa
yerimstar <yerimhuh@naver.com>
2020-07-13 22:39:09 +0900

    20200713 study - 셀프 넘버 수정

--- 4673.py
+++ 4673.py
@@ -1,3 +1,5 @@
+"""
+1번 방법 - 무식한 방법...
 numbers = [i for i in range(1,10001)]
 no_self_num = []
 for number in numbers:
@@ -13,3 +15,15 @@ for number in numbers:
 sort_difference = sorted(set(numbers).difference(set(no_self_num)))
 for d in sort_difference:
     print(d)
+
+"""
+# 2번 방법 - 조금 더 깔끔한 느낌
+num = set(range(1,10001))
+answer = set()
+for i in range(1,10001):
+    for j in str(i):
+        i += int(j)
+    answer.add(i)
+real = num-answer
+for i in sorted(real):
+    print(i)
