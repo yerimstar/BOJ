04f71eed5b48d5eebc4299a8bf73ad6fbb87aa38
yerimstar <yerimhuh@naver.com>
2020-07-15 00:13:46 +0900

    20200714 study - 체스판 다시 칠하기

---
+++ 1018.py
@@ -0,0 +1,33 @@
+import numpy as np
+M,N = map(int,input().split())
+chess = np.full((M,N),0)
+black = np.array(([1,0,1,0,1,0,1,0],[0,1,0,1,0,1,0,1])*4)
+white = np.array(([0,1,0,1,0,1,0,1],[1,0,1,0,1,0,1,0])*4)
+
+for m in range(M):
+    color = input()
+    for n in range(N):
+        if color[n] == "W":
+            chess[m][n] = 0
+        elif color[n] == "B":
+            chess[m][n] = 1
+
+final_count = []
+for m in range(M - 8 + 1):
+    for n in range(N - 8 + 1):
+        count_b = 0
+        count_w = 0
+        for i in range(m,m+8):
+            for j in range(n,n+8):
+                if black[i-m][j-n] != chess[i][j]:
+                    count_b += 1
+                elif white[i-m][j-n] != chess[i][j]:
+                    count_w += 1
+        final_count.append(count_b)
+        final_count.append(count_w)
+
+print(min(final_count))
+
+
+
+
