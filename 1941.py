885f6c26e39c2c8cce72fcba9f6e2eeee03e04ae
yerimstar <yerimhuh@naver.com>
2020-07-15 15:31:49 +0900

    20200715 study - 소문난 칠공주

--- 1941.py
+++ 1941.py
@@ -1,5 +1,9 @@
-girls = [list(input()) for _ in range(5)]
+def seven(count):
+    if count == len(dasom):
+        print()
+    else:
+        for i in range(4):
 
-def seven():
-    if
-        
+girls = [list(input()) for _ in range(5)]
+dasom = [(i,j) for i in range(5) for j in range(5) if girls[i][j] == 'S']
+print(dasom)
