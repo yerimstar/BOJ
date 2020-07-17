bfebb2c6efb5e44db11e827d8b472ef0f3333a01
yerimstar <yerimhuh@naver.com>
2020-07-17 22:24:20 +0900

    20200717 study - 벌집

--- 2292.py
+++ 2292.py
@@ -1,19 +1,11 @@
 N = int(input())
-num = [0 for _ in range(N)]
-for i in range(N//6):
-    print("i = ", i)
-    if i == 0:
-        num[i] = 1
-        x = 2
-    else:
-        for j in range(x,x+6*i):
-            print("j = ", j)
-            print("x+6*i",x+6*i)
-            num[j] = i
-            print(num[j])
-            if j == (x+6*i-1):
-                x = x + 6 * i
-
-print(num[N])
-
-
+count = 1
+range = 1
+temp = 1
+while(1):
+    if range >= N:
+        break
+    temp = 6*count
+    count += 1
+    range += temp
+print(count)
