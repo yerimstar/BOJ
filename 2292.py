ab0cb5e79789dde57cee988a4d44a510817a603d
yerimstar <yerimhuh@naver.com>
2020-07-17 16:19:38 +0900

    20200717 study

--- 2292.py
+++ 2292.py
@@ -1,3 +1,19 @@
 N = int(input())
-num = [0 for _ in range(100)]
-for i in range(100//6):
+num = [0 for _ in range(N)]
+for i in range(N//6):
+    print("i = ", i)
+    if i == 0:
+        num[i] = 1
+        x = 2
+    else:
+        for j in range(x,x+6*i):
+            print("j = ", j)
+            print("x+6*i",x+6*i)
+            num[j] = i
+            print(num[j])
+            if j == (x+6*i-1):
+                x = x + 6 * i
+
+print(num[N])
+
+
