bceaf4e2b38ed3e0b2e997b6a3e719b02ac0d9c2
yerimstar <yerimhuh@naver.com>
2020-07-18 16:12:38 +0900

    20200717 study - 골드바흐의 추측

--- 9020.py
+++ 9020.py
@@ -1,3 +1,5 @@
+"""
+# 시간초과
 def isPrime(n):
     if n!= 1:
         for i in range(2,n):
@@ -28,5 +30,25 @@ for i in range(T):
             min = min_prime
     for p in range(len(Prime)):
         if min == abs(Prime[p][0]-Prime[p][1]):
+            print(Prime[p][0],Prime[p][1])
+            break
+"""
+def isPrime(n):
+    a = [False,False] + [True]*(n-1)
+    primes = []
+    for i in range(2,n+1):
+        if a[i]:
+            primes.append(i)
+            for j in range(2*i,n+1,i):
+                a[j] = False
+    Prime = [(i,j) for i in primes for j in primes if i+j == n]
+    Diff = [abs(p[0]-p[1]) for p in Prime]
+    return Prime,Diff
+
+for _ in range(int(input())):
+    N = int(input())
+    Prime,Diff = isPrime(N)
+    for p in range(len(Prime)):
+        if  min(Diff) == abs(Prime[p][0]-Prime[p][1]):
             print(Prime[p][0],Prime[p][1])
             break
