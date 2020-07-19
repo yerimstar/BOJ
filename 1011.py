fcaec37e21fd45c663ee8ececd2408a05b3ebb23
yerimstar <yerimhuh@naver.com>
2020-07-19 18:47:17 +0900

    20200719 study - Fly me to the Alpha Centauri

--- 1011.py
+++ 1011.py
@@ -1,10 +1,15 @@
 T = int(input())
 for i in range(T):
     x,y = map(int,input().split())
-    count = 2
-    temp = [0,1,2]
-    length = 0
+    length = y-x
+    num = 1
     while(1):
-
-        if length == (y-x):
-            print(count)
+        if num**2 <= length < (num+1)**2:
+            break
+        num += 1
+    if num**2 == length:
+        print((num*2)-1)
+    elif num**2 < length <= (num**2)+num:
+        print(num*2)
+    else:
+        print((num*2)+1)
