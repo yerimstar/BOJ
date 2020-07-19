6e2ccb560c35a0bda41e7b725f7b3d70b02061ec
yerimstar <yerimhuh@naver.com>
2020-07-20 02:12:59 +0900

    20200719 study - 수 정렬하기 (메모리초과 + 런타임 에러)

--- 10989.py
+++ 10989.py
@@ -1,9 +1,10 @@
 import sys
 N = int(sys.stdin.readline())
-nums = [0]*(N)
+nums = [0]*10001
 print(nums)
 for i in range(N):
-    num = int(input())
+    num = int(sys.stdin.readline())
     nums[i] = num
 for i in sorted(nums):
-    print(i)
+    if i != 0:
+        print(i)
