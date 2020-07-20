730d596709e14f9b2e47bec1e81914ac81d8ffe9
yerimstar <yerimhuh@naver.com>
2020-07-21 00:54:37 +0900

    20200719 study - 단어 정렬

--- 1181.py
+++ 1181.py
@@ -2,11 +2,16 @@ import sys
 N = int(sys.stdin.readline())
 words = []
 for i in range(N):
-    words.append(sys.stdin.readline())
+    words.append(sys.stdin.readline().strip())
 words.sort(key = len)
+print(words)
+new = []
+for w in words:
+    if w not in new:
+        new.append(w)
+print(new)
 
-set_words = set(words)
-print(set_words)
+
 
 
 
