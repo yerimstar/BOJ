a63b4de640adce7f6e0e99bf7df4a00a474cdf9b
yerimstar <yerimhuh@naver.com>
2021-01-17 03:55:45 +0900

    20210117

--- 10989.py
+++ 10989.py
@@ -1,10 +1,5 @@
-import sys
-N = int(sys.stdin.readline())
-nums = [0]*10001
-print(nums)
-for i in range(N):
-    num = int(sys.stdin.readline())
-    nums[i] = num
-for i in sorted(nums):
-    if i != 0:
-        print(i)
+from sys import stdin
+num = list(int(stdin.readline()) for i in range(int(input())))
+num.sort()
+for n in num:
+    print(n)
