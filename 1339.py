6678386972e6e89df52d19deef7334aab4edb7b2
yerimstar <yerimhuh@naver.com>
2021-06-21 14:56:30 +0900

    20210621

--- 1339.py
+++ 1339.py
@@ -1,30 +1,34 @@
 N = int(input())
 num = list(range(10))
-alpha = []
+words = []
+alpha = {}
 tmp = {}
 
-for _ in range(N):
-    alpha.append(input())
-
-alpha.sort(key = lambda x : len(x), reverse=True)
-
 for i in range(N):
-    for j in range(len(alpha[i])):
-        val = alpha[i][j]
-        if len(tmp) == 0:
-            tmp[val] = str(max(num))
-            num.remove(max(num))
-        else:
-            if val in tmp:
-                continue
-            else:
-                tmp[val] = str(max(num))
-                num.remove(max(num))
+    word = input()
+    words.append(word)
+    for j in range(len(word)):
+        alpha.append((len(word)-j,word[j],0))
+
+for a in alpha:
+    print(a[1])
+
+
+alpha.sort(key=lambda x : (x[0],alpha.count(x[1])),reverse=True)
+print(alpha)
+
+for a in alpha:
+    if a[1] in tmp: # 알파벳과 0~9 매칭
+        continue
+    else:
+        tmp[a[1]] = str(max(num))
+        num.remove(max(num))
+
 result = 0
 for i in range(N):
     tmpresult = ""
-    for j in range(len(alpha[i])):
-        tmpresult += tmp[alpha[i][j]]
+    for j in range(len(words[i])):
+        tmpresult += tmp[words[i][j]]
     result += int(tmpresult)
-print(tmp)
+
 print(result)
