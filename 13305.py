b9c290b2b3f93eabdede2cde100f2953d09542eb
yerimstar <yerimhuh@naver.com>
2021-06-23 08:23:33 +0900

    20210623

--- 13305.py
+++ 13305.py
@@ -1,25 +1,13 @@
 N = int(input())
 distance = list(map(int,input().split()))
 money = list(map(int,input().split()))[:-1]
+minPrice = money[0]
+result = minPrice * distance[0]
 
-result = 0
-endIndex = len(distance)
-
-while True:
-    minMoney = min(money)
-    startIndex = money.index(minMoney)
-
-    for i in range(startIndex, endIndex):
-        result += (minMoney * distance[i])
-
-    if startIndex == 0:
-        break
-    else:
-        if startIndex > 1:
-            money = money[:startIndex]
-            endIndex = startIndex
-        else:
-            result += money[0] * distance[0]
-            break
-
+for i in range(1,N-1):
+    if money[i] < minPrice:
+        minPrice = money[i]
+    result += minPrice * distance[i]
 print(result)
+
+
