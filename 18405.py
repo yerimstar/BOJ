a3653c03978930f5605aee74f9072de0db362a5b
yerimstar <yerimhuh@naver.com>
2021-07-09 13:30:16 +0900

    210709

--- 18405.py
+++ 18405.py
@@ -1,24 +1,25 @@
-N, K = map(int,input().split())
-lst = [list(map(int,input().split())) for _ in range(N)]
+import sys
+from collections import deque
+
+N, K = map(int, sys.stdin.readline().split())
+lst = [list(map(int,sys.stdin.readline().split())) for _ in range(N)]
 S,X,Y = map(int,input().split())
+
 move = [[-1,0],[1,0],[0,-1],[0,1]]
-virus = [[(i,j)] for i in range(N) for j in range(N) for k in range(1,K+1) if lst[i][j] == k]
+virus = [[lst[i][j],i,j,0] for i in range(N) for j in range(N) if lst[i][j] != 0]
 
-print(virus)
-if S != 0:
-    for s in range(S):
-        for i in range(K):
-            tmp = []
-            for v in virus[i]:
-                for m in move:
-                    check1 = v[0] + m[0]
-                    check2 = v[1] + m[1]
-                    if (0 <= check1 < N) and (0 <= check2 < N):
-                        if lst[check1][check2] == 0:
-                            lst[check1][check2] = i + 1
-                            tmp.append((check1, check2))
-            virus[i] = tmp
+virus.sort() #lst[i][j] - 바이러스 값을 오름차순 정렬
+virus = deque(virus)
 
-print(lst)
-print(virus)
+while virus:
+    v,i,j,t = virus.popleft() #바이러스 1번부터 값을 넣어두기
+    if t == S:
+        break
+    else:
+        for m in move:
+            check1 = i + m[0]
+            check2 = j + m[1]
+            if (0 <= check1 < N) and (0 <= check2 < N) and (lst[check1][check2] == 0):
+                lst[check1][check2] = v
+                virus.append([v,check1,check2,t+1])
 print(lst[X-1][Y-1])
