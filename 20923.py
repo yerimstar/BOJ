22e20eaccf0aedef578b70b7d6d3e66b3ebd83fc
yerimstar <yerimhuh@naver.com>
2021-07-09 12:45:43 +0900

    상대방의 덱을 먼저 뒤집어야 하는데, 자신의 덱을 먼저 뒤집음...

--- 20923.py
+++ 20923.py
@@ -1,17 +1,21 @@
+import sys
 from collections import deque
-N,M = map(int,input().split())
+
+N,M = map(int,sys.stdin.readline().split())
 do_queue  = deque()
 su_queue = deque()
 
 for _ in range(N):
-    do,su = map(int,input().split())
+    do,su = map(int,sys.stdin.readline().split())
     do_queue.append(do)
     su_queue.append(su)
 
 do_check = 0
 su_check = 0
-do_tmp = []
-su_tmp = []
+
+do_tmp = deque()
+su_tmp = deque()
+
 for m in range(M):
     if m % 2 == 0:
         do_check = do_queue.pop()
@@ -27,17 +31,25 @@ for m in range(M):
         print("do")
         exit()
 
-    if do_check + su_check == 5:
-        for i in range(len(su_tmp)):
-            su_queue.appendleft(su_tmp[i])
-        for i in range(len(do_tmp)):
-            su_queue.appendleft(do_tmp[i])
+    if (do_check + su_check == 5) and (do_check != 0) and (su_check != 0):
+        if do_tmp:
+            su_queue.extendleft(do_tmp)
+            do_check = 0
+            do_tmp = deque()
+        if su_tmp:
+            su_queue.extendleft(su_tmp)
+            su_check = 0
+            su_tmp = deque()
 
     elif (do_check == 5) or (su_check == 5):
-        for i in range(len(do_tmp)):
-            do_queue.appendleft(do_tmp[i])
-        for i in range(len(su_tmp)):
-            do_queue.appendleft(su_tmp[i])
+        if su_tmp:
+            do_queue.extendleft(su_tmp)
+            su_check = 0
+            su_tmp = deque()
+        if do_tmp:
+            do_queue.extendleft(do_tmp)
+            do_check = 0
+            do_tmp = deque()
 
 if len(do_queue) > len(su_queue):
     print("do")
