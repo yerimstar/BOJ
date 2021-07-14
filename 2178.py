588f3272fb9beb182e3e388994154c647f1f19b1
yerimstar <yerimhuh@naver.com>
2021-07-15 05:54:51 +0900

    210715

--- 2178.py
+++ 2178.py
@@ -1,6 +1,29 @@
+# 미로 탐색
 import sys
-N,M = map(int,sys.stdin.readline())
+from collections import deque
+N,M = map(int,sys.stdin.readline().split())
 graph = []
-for i in range(N):
-    for j in range(M):
-        graph[i][j] =
+for _ in range(N):
+    graph.append(list(map(int,input())))
+
+dx = [-1,1,0,0] #상하좌우 표현
+dy = [0,0,-1,1]
+
+def bfs(x,y):
+    queue = deque()
+    queue.append((x,y)) # 시작지점
+    while queue:
+        x,y = queue.popleft()
+        for i in range(4): # 상하좌우 탐색
+            tx = x+dx[i]
+            ty = y+dy[i]
+            if tx < 0 or N <= tx or ty < 0 or M <= ty: # N X M안에 존재해야 함
+                continue
+            if graph[tx][ty] == 0: # 0값이라면 지나갈 수 없음
+                continue
+            if graph[tx][ty] == 1:
+                graph[tx][ty] = graph[x][y] + 1 # tx,ty까지 이동한 최단거리값 저장
+                queue.append((tx,ty))
+    return graph[N-1][M-1] # 마지막 칸의 최단경로 값 반환
+
+print(bfs(0,0))
--- 2644.py
+++ 2644.py
@@ -1,4 +1,6 @@
 import sys
+from collections import deque
+
 n = int(sys.stdin.readline())
 num1,num2 = map(int ,sys.stdin.readline().split())
 m = int(sys.stdin.readline())
@@ -9,37 +11,18 @@ for _ in range(m):
     graph[m2].append(m1)
 print(graph)
 
-def dfs(start_node):
+def check(start_node):
     visited = list()
-    answer = list()
-    stack = list()
+    queue = deque()
+    queue.append(start_node)
 
-    stack.append(start_node)
-    visited.append(start_node)
+    while queue:
+        node = queue.popleft()
+        print("node = ",node)
+        if node not in visited:
+            visited.append(node)
+            queue.extend(graph[node])
 
-    while stack:
-        node = stack.pop()
-        answer.append(node)
 
-        for i in range(len(graph[node])):
-            if i not in visited:
-                if graph[node][i] == 2:
-                    stack.append(i)
-                    visited.append(i)
-                    break
-                elif graph[node][i] == 1:
-                    stack.append(i)
-                    visited.append(i)
-                    break
-    if start_node == num1:
-        if num2 in answer:
-            return answer.index(num2)
-        else:
-            return -1
-    elif start_node == num2:
-        if num1 in answer:
-            return answer.index(num1)
-        else:
-            return -1
 
-print(max(dfs(num1),dfs(num2)))
+print(check(num1))
