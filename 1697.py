f50ee849a2e24fbffc93781ec870ef180131e5c6
yerimstar <yerimhuh@naver.com>
2021-07-21 22:51:34 +0900

    210721 - 해결

--- 1697.py
+++ 1697.py
@@ -7,17 +7,21 @@ graph를 visited처럼 사용함. 기존 값 0 -> 방문하면 1
 import sys
 from collections import deque
 N,K = map(int,sys.stdin.readline().split())
-graph = [0 for _ in range(100001)]
+graph = [0 for _ in range(100002)]
 graph[N] = 1
 graph[K] = 1
 
 def bfs(x,y):
+    if x == y: # 조건 추가 !!
+        return 0
     queue = deque()
     cnt = 1
     queue.append([x,cnt])
     while queue:
         x,cnt = queue.popleft()
         for tx in (x-1,x+1,x*2):
+            if tx < 0 or tx > 100001:
+                continue
             if tx == y:
                 return cnt
             if graph[tx] == 0:
