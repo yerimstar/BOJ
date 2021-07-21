6493046e34fb71c1a53274fe97ad6d6560cf003a
yerimstar <yerimhuh@naver.com>
2021-07-21 22:51:54 +0900

    210721 - 해결

--- 1697.py
+++ 1697.py
@@ -7,7 +7,7 @@ graph를 visited처럼 사용함. 기존 값 0 -> 방문하면 1
 import sys
 from collections import deque
 N,K = map(int,sys.stdin.readline().split())
-graph = [0 for _ in range(100002)]
+graph = [0 for _ in range(100002)] # 런타임에러 해결
 graph[N] = 1
 graph[K] = 1
 
@@ -20,7 +20,7 @@ def bfs(x,y):
     while queue:
         x,cnt = queue.popleft()
         for tx in (x-1,x+1,x*2):
-            if tx < 0 or tx > 100001:
+            if tx < 0 or tx > 100001: # 런타임에러 해결
                 continue
             if tx == y:
                 return cnt
