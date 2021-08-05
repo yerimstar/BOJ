7402fe652f8f6aa7db768a512a27842e844dc61f
yerimstar <yerimhuh@naver.com>
2021-08-05 15:20:35 +0900

    210805

--- 9095.py
+++ 9095.py
@@ -1,4 +1,13 @@
 # 1,2,3 더하기
 T = int(input())
+lst = []
+
 for _ in range(T):
-    dp = 
+    num = [0, 1, 2, 4]
+    N = int(input())
+    for i in range(4,N+1):
+        num.append(num[i-1]+num[i-2]+num[i-3])
+    lst.append(num[N])
+
+for l in lst:
+    print(l)
