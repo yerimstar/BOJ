0559529a6ec9a4a3ec662553603b7ada868d72eb
yerimstar <yerimhuh@naver.com>
2021-08-19 17:28:44 +0900

    210819

--- 1149.py
+++ 1149.py
@@ -1,26 +1,18 @@
 # RGB거리
 import sys
 N = int(sys.stdin.readline())
-home = [[0] * 3] * N
-color = [0] * N
+color = []
+home = []
 for i in range(N):
-    home[i] = list(map(int,sys.stdin.readline().rstrip().split()))
+    home.append(list(map(int,sys.stdin.readline().rstrip().split())))
 
-for i in range(0,3):
-    check = i
-    for j in range(N):
-        if check == 0:
-            home_color = min(home[j][1], home[j][2])
-            check = home[j].index(home_color)
-            color[i] += home_color
-        elif check == 1:
-            home_color = min(home[j][0], home[j][2])
-            check = home[j].index(home_color)
-            color[i] += home_color
-        elif check == 2:
-            home_color = min(home[j][0], home[j][1])
-            check = home[j].index(home_color)
-            color[i] += home_color
-        print(home_color)
-print(color)
-print(min(color))
+color.append([home[0][0],home[0][1],home[0][2]])
+
+for i in range(1,N):
+    color.append([min(color[i - 1][1], color[i - 1][2]) + home[i][0],min(color[i - 1][0], color[i - 1][2]) + home[i][1],min(color[i - 1][0], color[i - 1][1]) + home[i][2]])
+
+print(min(color[N-1]))
+
+# 궁금한 점
+# color = [[0] * 3] * N 이렇게 두고 코드를 짰더니 ..
+# color[1],color[2]값이 동시에 변경됨...이유가 뭘까..ㅜㅜ
