dfc3290fea24cf01a61579892b4470148384bc1d
yerimstar <yerimhuh@naver.com>
2021-08-19 20:06:29 +0900

    210819

--- 11722.py
+++ 11722.py
@@ -1,12 +1,14 @@
 # 가장 긴 감소하는 부분 수열
+# 구글링함...값을 저장해준다는게 핵심..!
+# 조건에서 수열이라는 것을 명시해줌!
 import sys
 N = int(sys.stdin.readline())
 arr = list(map(int,sys.stdin.readline().split()))
-len = [1 for _ in range(N)]
+length = [1 for _ in range(N)]
 
 for i in range(N):
     for j in range(i):
         if arr[j] > arr[i]:
-            len[i] = max(len[i],len[j]+1)
+            length[i] = max(length[i],length[j]+1)
 
-print(max(len))
+print(max(length))
---
+++ 1890.py
@@ -0,0 +1,23 @@
+# 점프
+# 경로 찾는 문제랑 유사...이전 값들을 저장해두기!
+import sys
+N = int(sys.stdin.readline())
+board = []
+visited = [[0]*N for _ in range(N)]
+visited[0][0] = 1
+
+for i in range(N):
+    board.append(list(map(int,sys.stdin.readline().split())))
+
+for i in range(N):
+    for j in range(N):
+        if i == N-1 and j == N-1:
+            break
+        right = j + board[i][j]
+        down = i + board[i][j]
+        # 범위 체크
+        if right < N:
+            visited[i][right] += visited[i][j]
+        if down < N:
+            visited[down][j] += visited[i][j]
+print(visited[i][j])
