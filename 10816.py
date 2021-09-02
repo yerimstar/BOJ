1002ae34981610dd56a4bca71ee78a3faa287840
yerimstar <yerimhuh@naver.com>
2021-09-02 15:41:52 +0900

    210902

--- 10816.py
+++ 10816.py
@@ -1,11 +1,11 @@
-# 1번 시도 - 시간 초과
+#2번 시도 - 시간 초과
 import sys
 N = int(sys.stdin.readline())
 N_list = list(map(int,sys.stdin.readline().split()))
 M = int(sys.stdin.readline())
 M_list = list(map(int,sys.stdin.readline().split()))
-result = []
 for m in M_list:
-    result.append(N_list.count(m))
-for r in result:
-    print(r,end=' ')
+    print(N_list.count(m),end = ' ')
+#=> list의 count가 시간을 많이 쓰는 듯..이 부분을 해결해야됨
+
+
