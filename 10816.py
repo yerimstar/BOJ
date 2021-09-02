9cd2341fc5921c2e8b6ec98c34e70254dca0e090
yerimstar <yerimhuh@naver.com>
2021-09-02 17:23:14 +0900

    210902

--- 10816.py
+++ 10816.py
@@ -5,13 +5,15 @@ card1 = list(map(int,sys.stdin.readline().split()))
 M = int(sys.stdin.readline().rstrip())
 card2 = list(map(int,sys.stdin.readline().split()))
 
-result = []
+counts = dict()
+for card in card1:
+    if card in counts:
+        counts[card] += 1
+    else:
+        counts[card] = 1
+
 for card in card2:
-    print(card)
-    start = card1.index(card)
-    end = len(card1)
-    while(start <= end):
-        count = 0
-        mid = (start + end) // 2
-        for c in card1:
-            count +=
+    if card in counts:
+        print(counts[card],end = ' ')
+    else:
+        print(0,end = ' ')
