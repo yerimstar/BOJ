800fd5ec0d8b1b430bbd4c3917aa62e801e8c954
yerimstar <yerimhuh@naver.com>
2021-11-10 23:49:14 +0900

    211110

--- 1339.py
+++ 1339.py
@@ -8,22 +8,22 @@ new = {}
 for _ in range(N):
     words.append(input())
 
-for word in words:
+for word in words: # 각 알파벳의 자릿수를 계산한다.
     for i in range(len(word)):
         if word[i] in tmp:
             tmp[word[i]] += (10**(len(word)-i-1))
         else:
             tmp[word[i]] = (10**(len(word)-i-1))
 
-tmp = sorted(tmp.items(), key = lambda x : x[1], reverse= True)
+tmp = sorted(tmp.items(), key = lambda x : x[1], reverse= True) # 자릿수값 기준으로 내림차순 정렬
 
 num = 9
-for i in range(len(tmp)):
+for i in range(len(tmp)): # 자릿수값이 큰 순서대로 숫자를 부여해준다.
     new[tmp[i][0]] = num
     num -= 1
 
 result = 0
-for word in words:
+for word in words: # 알파벳을 숫자로 변환
     tmpresult = ""
     for w in word:
         tmpresult += str(new[w])
