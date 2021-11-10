37406b72aedb877aabe9ce16d3a4768c78f84273
yerimstar <yerimhuh@naver.com>
2021-11-10 23:41:07 +0900

    211110

--- 1715.py
+++ 1715.py
@@ -10,7 +10,7 @@ for _ in range(N):
 result = 0
 tmp = 0
 
-for i in range(N-1):
+for i in range(N-1): # N-1만큼 덧셈이 이루어지기 때문에 N-1만큼 반복문 수행
     tmp = (que.get()+que.get()) # 큐 데이터 추출
     result += tmp # 결과값 저장
     que.put(tmp) # 데이터 2개를 더한 값을 우선순위 큐에 다시 넣어준다
