81d2a5d375a342217944ba744b5c2dad8d53778b
yerimstar <yerimhuh@naver.com>
2021-11-23 18:58:27 +0900

    211122

--- 5052.py
+++ 5052.py
@@ -1,19 +1,21 @@
 # 전화번호 목록
 import sys
 
+def fcheck(phone):
+    for i in range(len(phone)-1):
+        prefix = phone[i] # 접두어
+        check = phone[i+1][:len(prefix)] # 접두어만 비교하기 위해 split
+        if prefix == check: # 접두어가 동일한 경우 일관성이 없다고 판단
+            print("NO")
+            return
+    print("YES")
+
+
 T = int(sys.stdin.readline().rstrip()) # 테스트 케이스 개수
 for _ in range(T):
     phone = list()
     N = int(sys.stdin.readline().rstrip()) # 전화번호 수
     for _ in range(N):
-        phone.append(sys.stdin.readline().rstrip())
-    phone.sort()
-    check = 0
-    for p in phone:
-        for x in phone:
-            if p in x and p != x:
-                print("NO")
-                check += 1
-                break
-    if check == 0:
-        print("YES")
+        phone.append((sys.stdin.readline().rstrip()))
+    phone.sort() # 전화번호 정렬하기
+    fcheck(phone)
