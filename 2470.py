54d5ea81c59eaffde659170a2c7fbe335fdfc65f
yerimstar <yerimhuh@naver.com>
2021-11-25 18:41:49 +0900

    211125 - 최종

--- 2470.py
+++ 2470.py
@@ -1,11 +1,23 @@
 # 두 용액
 import sys
 
-N = sys.stdin.readline().strip() # 전체 용액의 수
+N = int(sys.stdin.readline().strip()) # 전체 용액의 수
 
-solution = list(map(int,sys.stdin.readline().split())) # 용액
+solution = sorted(map(int,sys.stdin.readline().split())) # 용액
 
-mixed_solution = list(set([(min(x,y),max(x,y),abs(x+y)) for x in solution for y in solution if x != y]))
-mixed_solution.sort(key=lambda x : x[2])
-print(mixed_solution[0][0],mixed_solution[0][1])
+start = 0
+end = N-1
+final = list()
+while start < end:
+    mixture = [solution[start],solution[end],abs(solution[start] + solution[end])] # 혼합값
+    final.append(mixture)
+    sum = solution[start] + solution[end]
+    if sum > 0: # 합이 양수이므로 큰 값을 줄여줘야 한다
+        end = end -1
+    elif sum < 0: # 합이 음수이므로 작은 값을 키워줘야 한다
+        start = start + 1
+    elif sum == 0: # 0인 경우 break
+        break
 
+final.sort(key=lambda x : x[2]) # 절댓값 기준으로 정렬한다.
+print(final[0][0],final[0][1])
