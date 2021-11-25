6255d7e3f6ec258e81748043677943c3b07cfc18
yerimstar <yerimhuh@naver.com>
2021-11-25 17:34:58 +0900

    211125 - 틀렸습니다

--- 1202.py
+++ 1202.py
@@ -1,17 +1,18 @@
-N,K = map(int,input().split())
-jewerly = []
-bag = []
-money = 0
+# 보석 도둑
+import sys
+N,K = map(int,sys.stdin.readline().split())
+jewerly = list()
+bag = list()
 
-for i in range(N):
-    M,V = map(int,input().split())
-    jewerly.append((M,V))
+for _ in range(N):
+    M,V = map(int,sys.stdin.readline().split())
+    jewerly.append([M,V])
 
 jewerly.sort(key = lambda x : x[0])
 print(jewerly)
 
-for i in range(K):
-    bag.append(int(input()))
+for _ in range(K):
+    bag.append(int(sys.stdin.readline().strip()))
 
 for i in range(K):
     for j in range(N):
---
+++ 2470.py
@@ -0,0 +1,18 @@
+# 두 용액
+import sys
+N = sys.stdin.readline().strip() # 전체 용액의 수
+
+solution = list(map(int,sys.stdin.readline().split())) # 용액
+
+acid_solution = [x for x in solution if x > 0]
+alkali_solution = [x for x in solution if x < 0]
+
+if len(alkali_solution) == 0: # 산성 용액만 있는 경우 -> 양수
+    acid_solution = sorted(acid_solution) # 산성 용액 오름차순 정렬
+    print(acid_solution[0],acid_solution[1])
+elif len(acid_solution) == 0: # 알칼리성 용액만 있는 경우 -> 음수
+    alkali_solution = sorted(alkali_solution) # 알칼리 용액 오름차순 정렬
+    print(alkali_solution[-2],alkali_solution[-1])
+else: # 두 용액 모두 있는 경우
+    solution = sorted(solution)
+    print(solution[0],solution[-1])
