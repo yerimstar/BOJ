511f3416509a7c159c834ead5950e09be8ad3a86
yerimstar <yerimhuh@naver.com>
2022-02-09 17:46:22 +0900

    220209

--- 18310.py
+++ 18310.py
@@ -2,10 +2,8 @@
 import sys
 n = int(sys.stdin.readline().strip())
 house = list(map(int,sys.stdin.readline().split()))
-tmp = []
-for i in range(1,max(house)+1):
-    sum = 0
-    for h in house:
-        sum += abs(h-i)
-    tmp.append([i,sum])
-print(min(tmp,key = lambda x: x[1])[0])
+house.sort()
+if n % 2 == 0:
+    print(house[n//2-1])
+else:
+    print(house[n//2])
