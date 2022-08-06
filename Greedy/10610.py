f3128101675d5c25e6023437f4ec6534c488ce4a
yerimstar <yerimhuh@naver.com>
2022-08-06 15:55:35 +0900

    [220806]

--- Greedy/10610.py
+++ Greedy/10610.py
@@ -1,10 +1,17 @@
 # 30
-from itertools import permutations
 n = input()
-lst = list(permutations(n,len(n)))
-result = -1
-for l in lst:
-    num = int("".join(l))
-    if num % 30 == 0 and l[0] != 0:
-        result = max(result,num)
-print(result)
+# 10의 배수 판별법 = 일의 자리 숫자가 0인 수
+if "0" not in n:
+    print(-1)
+# 10의 배수 조건 성립, 3의 배수 조건 판별
+else:
+    # 각 자리 수의 합 구하기
+    nSum = sum(map(int,n))
+
+    # 3의 배수 판별법 = 각 자리 수의 합이 3의 배수
+    if nSum % 3 == 0:
+        # 가장 큰 수를 구해야 하기 때문에 내림차순 정렬
+        n = sorted(n,reverse=True)
+        print("".join(n))
+    else:
+        print(-1)
