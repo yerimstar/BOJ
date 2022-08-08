386f34032ef17cb0f2eee5055b83078c409952d6
yerimstar <yerimhuh@naver.com>
2022-08-08 10:10:12 +0900

    [220808] 오답

--- Greedy/2012.py
+++ Greedy/2012.py
@@ -8,13 +8,5 @@ nums = [i+1 for i in range(N)]
 score = 0
 for _ in range(N):
     rank = int(input())
-    if rank in ranks:
-        changeRank = sorted(nums)[0]
-        ranks.append(changeRank)
-        nums.remove(changeRank)
-        score += abs(changeRank-rank)
-    else:
-        ranks.append(rank)
-        nums.remove(rank)
-
-print(score)
+    ranks.append(rank)
+print(sum(nums)-sum(ranks))
