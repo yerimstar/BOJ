b3630b721be1ac02ca4786527758d48779c0af36
yerimstar <yerimhuh@naver.com>
2022-08-08 11:02:32 +0900

    [220808] 시간초과

--- Greedy/2012.py
+++ Greedy/2012.py
@@ -9,4 +9,15 @@ score = 0
 for _ in range(N):
     rank = int(input())
     ranks.append(rank)
-print(sum(nums)-sum(ranks))
+
+ranks.sort()
+check = list(set(nums)-set(ranks))
+
+for i in range(N-1):
+    if ranks[i] == ranks[i-1]:
+        score += abs(check[0]-ranks[i])
+        check.remove(check[0])
+    else:
+        pass
+
+print(score)
