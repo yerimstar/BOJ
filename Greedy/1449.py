011ab71358c1eed128a0210837cdacaab8e9d317
yerimstar <yerimhuh@naver.com>
2022-08-08 13:24:33 +0900

    [220808]

--- Greedy/1449.py
+++ Greedy/1449.py
@@ -1,5 +1,17 @@
 # 수리공 항승
+import sys
 # N - 물이 새는 곳의 개수, L - 테이프 길이
-N,L = map(int,input().split())
+N,L = map(int,sys.stdin.readline().split())
 # 물이 새는 곳의 위치
-spot = map(int,input().split())
+spot = list(map(int,sys.stdin.readline().split()))
+spot = sorted(spot)
+
+# 테이프 개수
+tapeCnt = 1
+check = 0
+for i in range(1,N):
+    # L-1 = 테이프 길이 - 0.5*2
+    if abs(spot[i]-spot[check]) > L-1:
+        tapeCnt += 1
+        check = i
+print(tapeCnt)
