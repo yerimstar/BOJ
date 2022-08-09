552f7039e0a0ad411c1ec0e8a9633d7ac8642dd2
yerimstar <yerimhuh@naver.com>
2022-08-09 12:08:59 +0900

    [220809]

--- BFS,DFS/1012.py
+++ BFS,DFS/1012.py
@@ -1,11 +1,12 @@
 # 유기농 배추
 # 인접 행렬을 사용해야 할 것 같음
-# 테스트 케이스
+import sys
+sys.setrecursionlimit(10**6)
 def dfs(graph,y,x):
     # print("y = ",y,"x = ",x)
     if x <= -1 or x >= m or y <= -1 or y >= n:
         return False
-    # print("graph[y][x] = ", graph[y][x])x
+    # print("graph[y][x] = ", graph[y][x])
     if graph[y][x] == 1:
         # 방문 처리
         graph[y][x] = 0
@@ -15,7 +16,7 @@ def dfs(graph,y,x):
         dfs(graph,y+1,x)
         return True
     return False
-
+# 테스트 케이스
 T = int(input())
 for _ in range(T):
     # m = 배추밭 가로길이, n = 배추밭 새로길이, k = 배추가 심어져있는 위치 개수
