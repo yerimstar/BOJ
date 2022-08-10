c675b017bd131b4dd6358b39a229e717a00d8af5
yerimstar <yerimhuh@naver.com>
2022-08-10 11:59:12 +0900

    [220810]

--- DP/2579.py
+++ DP/2579.py
@@ -7,11 +7,13 @@ score = [0,]
 for _ in range(n):
     score.append(int(sys.stdin.readline()))
 
-d = [0] * (n+1)
-d[1] = score[1]
-d[2] = d[1] + score[2]
+if n == 1:
+    print(score[1])
+else:
+    d = [0] * (n + 1)
+    d[1] = score[1]
+    d[2] = d[1] + score[2]
 
-for i in range(3,n+1):
-    d[i] = max(d[i-2],d[i-3]+score[i-1])+score[i]
-print(d[n])
-
+    for i in range(3, n + 1):
+        d[i] = max(d[i - 2], d[i - 3] + score[i - 1]) + score[i]
+    print(d[n])
