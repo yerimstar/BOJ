f4356a1faed8307bc1743d30d06b33c0f0a23099
yerimstar <yerimhuh@naver.com>
2022-08-13 12:33:08 +0900

    [220813] 오답

--- BFS,DFS/1987.py
+++ BFS,DFS/1987.py
@@ -1,11 +1,7 @@
 # 알파벳
 import sys
-
-def dfs(graph,y,x,visited):
-    print("y = ",y,"x = ",x)
-    # 범위 안에 존재하는지 검사
-    if y <= -1 or y >= R or x <= -1 or x >= C:
-        return False
+sys.setrecursionlimit(10**6)
+def dfs(graph,y,x,visited,move):
     # 방문한적 없는 알파벳인 경우
     if not visited[y][x] and graph[y][x] not in lst:
         lst.append(graph[y][x])
@@ -14,18 +10,24 @@ def dfs(graph,y,x,visited):
     else:
         return False
     # 상하좌우
-    move = [[0,-1],[0,1],[-1,0],[1,0]]
     for my,mx in move:
         dy = y + my
         dx = x + mx
-        print("dy = ",dy,"dx = ",dx)
-        dfs(graph,dy,dx,visited)
+        # 범위 안에 존재하는지 검사
+        if dy <= -1 or dy >= R or dx <= -1 or dx >= C:
+            continue
+        dfs(graph,dy,dx,visited,move)
 
 R,C = map(int,sys.stdin.readline().split())
 graph = [list(sys.stdin.readline().rstrip()) for _ in range(R)]
-visited = [[0 for _ in range(C)] for _ in range(R)]
-lst = []
 # 좌측 상단에서 시작 (0,0)
-dfs(graph,0,0,visited)
-print(graph)
-print(len(lst))
+
+move1 = [[0, -1], [0, 1], [-1, 0], [1, 0]]
+move2 = [[-1,0],[1,0],[0,-1],[0,1]]
+result = 0
+for move in [move1,move2]:
+    lst = []
+    visited = [[0 for _ in range(C)] for _ in range(R)]
+    dfs(graph,0,0,visited,move)
+    result = max(result,len(lst))
+print(result)
