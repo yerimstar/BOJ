8a40bb60dc31c91fc194992c167b4a1d369120ef
yerimstar <yerimhuh@naver.com>
2022-08-19 16:09:15 +0900

    [220819] 이코테 기출 Greedy

--- Greedy/만들 수 없는 금액.py
+++ Greedy/만들 수 없는 금액.py
@@ -2,16 +2,11 @@ import sys
 n = int(sys.stdin.readline())
 lst = list(map(int,sys.stdin.readline().split()))
 lst.sort()
-if lst[0] > 1:
-    print(1)
-    exit()
-else:
-    result = 1
-for i in range(1,len(lst)):
-    if result < lst[i]:
-        print(result+1)
-        exit()
-    else:
-        result += lst[i]
 
-print(result+1)
+# 초기에 1을 더하고 시작한다
+result = 1
+for l in lst:
+    if result < l:
+        break
+    result += l
+print(result)
