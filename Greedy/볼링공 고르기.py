4e3871520fa87c2ab83d5e31b2a91f2698324df5
yerimstar <yerimhuh@naver.com>
2022-08-19 16:36:10 +0900

    [220819] 이코테 기출 Greedy

--- Greedy/볼링공 고르기.py
+++ Greedy/볼링공 고르기.py
@@ -3,8 +3,8 @@ n,m = map(int,sys.stdin.readline().split())
 lst = list(map(int,sys.stdin.readline().split()))
 lst.sort()
 result = 0
-for i in range(len(lst)):
-    for j in range(i+1,len(lst)):
+for i in range(n):
+    for j in range(i+1,n):
         if lst[i] != lst[j]:
             result += 1
 print(result)
