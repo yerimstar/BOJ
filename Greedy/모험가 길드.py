2a1e8fa9f1ac4bc4dc45898358c0cc94e3e081d8
yerimstar <yerimhuh@naver.com>
2022-08-19 15:05:00 +0900

    [220819] 이코테 기출 Greedy

--- Greedy/모험가 길드.py
+++ Greedy/모험가 길드.py
@@ -2,15 +2,18 @@ import sys
 n = int(sys.stdin.readline())
 lst = list(map(int,sys.stdin.readline().split()))
 lst.sort()
+# 그룹 수
 group = 0
-check = lst[0]
+# 현재 그룹에 있는 인원 수
 cnt = 0
-for i in range(len(lst)):
-    if cnt != check and check == lst[i]:
-        cnt += 1
-    if cnt == check:
+for l in lst:
+    # 현재 그룹에 인원 추가
+    cnt += 1
+    # 현재 그룹에 있는 인원수가 현재 비교하고자 하는 공포도보다 크거나 같을 경우
+    # 그룹을 형성할 수 있음
+    if cnt >= l:
+        # 그룹 형성
         group += 1
+        # 새로운 그룹을 만들어야 하므로 그룹 인원 초기화
         cnt = 0
-        if i != len(lst) -1:
-            check = lst[i+1]
 print(group)
