d1dd7154e6e34e29f75ba0b81417fb2ded5e9dc2
yerimstar <yerimhuh@naver.com>
2022-09-23 20:44:31 +0900

    [220923] 완료

--- BFS,DFS/2206.py
+++ BFS,DFS/2206.py
@@ -1,50 +1,40 @@
 # 벽 부수고 이동하기
+# 입력
 import sys
-import copy
 from collections import deque
 
-N,M = map(int,sys.stdin.readline().split())
-graph = []
-# 맵 생성
-for _ in range(N):
-    graph.append(list(map(int,sys.stdin.readline().strip())))
-
-# 벽이 있는 위치 저장
-wall = []
-for i in range(N):
-    for j in range(M):
-        if graph[i][j] == 1:
-            wall.append([i,j])
-            graph[i][j] = 0
-        else:
-            graph[i][j] = 1
-
-move = [[0,1],[0,-1],[1,0],[-1,0]]
-
-def bfs(map_lst):
-    queue = deque([(0,0)])
-    while queue:
-        x,y = queue.popleft()
+n,m = map(int,sys.stdin.readline().split())
+graph = [list(map(int,sys.stdin.readline().strip())) for _ in range(n)]
+visited = [[[0]*2 for _ in range(m)] for _ in range(n)]
+# 함수
+def bfs():
+    q = deque()
+    # x, y, crashed
+    q.append((0,0,0))
+    # 방문처리 - 시작칸 포함
+    visited[0][0][0] = 1
+    while q:
+        x,y,crashed = q.popleft()
+        # 끝나는 칸 도착
+        if x == n-1 and y == m-1:
+            return visited[x][y][crashed]
         for i in range(4):
             dx = x + move[i][0]
             dy = y + move[i][1]
-            if dx < 0 or dx >= N or dy < 0 or dy >= M:
-               continue
-            if map_lst[dx][dy] == 1:
-                queue.append([dx, dy])
-                map_lst[dx][dy] = map_lst[x][y] + 1
-    return map_lst[N-1][M-1]
-
-min_num = 1000
-for w in wall:
-    map_lst = copy.deepcopy(graph)
-    map_lst[w[0]][w[1]] = 1
-    num = bfs(map_lst)
-    if num > 1 and num < min_num :
-        min_num = num
-if min_num == 1000:
-    print(-1)
-else:
-    print(min_num)
+            # 맵 안에 존재하는지 검사
+            if dx < 0 or dx >= n or dy < 0 or dy >= m:
+                continue
+            # 벽이 아니고, 방문하기 전인 상황
+            if graph[dx][dy] == 0 and visited[dx][dy][crashed] == 0:
+                visited[dx][dy][crashed] = visited[x][y][crashed] + 1
+                q.append((dx,dy,crashed))
+            # 벽이고, 벽을 아직 안 부순 상황
+            elif graph[dx][dy] == 1 and crashed == 0:
+                visited[dx][dy][1] = visited[x][y][crashed] + 1
+                q.append((dx,dy,1))
+    return -1
 
 
+# 출력
+move = [[0,1],[0,-1],[1,0],[-1,0]]
+print(bfs())
