bff126c201b715da54a19dd541320ca93ec45d18
yerimstar <yerimhuh@naver.com>
2022-09-25 15:53:27 +0900

    [220925] 완료

--- BFS,DFS/5014.py
+++ BFS,DFS/5014.py
@@ -5,10 +5,11 @@ from collections import deque
 F,S,G,U,D = map(int,sys.stdin.readline().split())
 queue = deque([S])
 visited = [0] * (F+1)
+visited[S] = 1
 while queue:
     now = queue.popleft()
     if now == G:
-        print(visited[now])
+        print(visited[now]-1)
         exit()
     for stair in (now+U,now-D):
         if 0 < stair <= F and visited[stair] == 0:
