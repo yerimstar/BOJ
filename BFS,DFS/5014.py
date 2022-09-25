05ff200192b68d59b84f5179b42e4a2f54ebdc29
yerimstar <yerimhuh@naver.com>
2022-09-25 15:49:33 +0900

    [220925] 66% 틀렸습니다

---
+++ BFS,DFS/5014.py
@@ -0,0 +1,17 @@
+# 스타트링크
+import sys
+from collections import deque
+
+F,S,G,U,D = map(int,sys.stdin.readline().split())
+queue = deque([S])
+visited = [0] * (F+1)
+while queue:
+    now = queue.popleft()
+    if now == G:
+        print(visited[now])
+        exit()
+    for stair in (now+U,now-D):
+        if 0 < stair <= F and visited[stair] == 0:
+            queue.append(stair)
+            visited[stair] = visited[now] + 1
+print("use the stairs")
