cb5aefa9d2a8ac42247bb3a00a9c0b5f5f7889f7
yerimstar <yerimhuh@naver.com>
2022-10-15 18:03:09 +0900

    [221015] 완료

--- BinarySearch/10815.py
+++ BinarySearch/10815.py
@@ -3,21 +3,21 @@ import sys
 
 def BinarySearch(s):
     left = 0
-    right = M-1
+    right = N-1
     while left <= right:
         mid = (left+right)//2
-        if sang[mid] == s:
+        if card[mid] == s:
             lst.append(1)
-            break
-        elif sang[mid] > s:
+            return
+        elif card[mid] > s:
             right = mid - 1
-        elif sang[mid] < s:
+        elif card[mid] < s:
             left = mid + 1
     lst.append(0)
 
 N = int(sys.stdin.readline().strip())
-sang = list(map(int,sys.stdin.readline().split()))
-sang.sort()
+card = list(map(int,sys.stdin.readline().split()))
+card.sort()
 M = int(sys.stdin.readline().strip())
 check = list(map(int,sys.stdin.readline().split()))
 lst = []
