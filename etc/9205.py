d37f1cc70ba5f2226aac6d253e02de2a20398981
yerimstar <yerimhuh@naver.com>
2022-11-01 20:50:53 +0900

    [221101] 완료

--- etc/9205.py
+++ etc/9205.py
@@ -1,23 +1,35 @@
 # 맥주 마시면서 걸어가기
 import sys
-import math
+from collections import deque
+
+def bfs():
+    queue = deque()
+    queue.append([conv[0][0],conv[0][1]])
+    visited[0] = True
+    while queue:
+        x,y = queue.popleft()
+        # 현재 위치와 도착 위치 사이의 거리가 1000 이하이면 -> happy
+        if abs(x-ex) + abs(y-ey) <= 1000:
+            print("happy")
+            return
+        for i in range(n+1):
+            if not visited[i]:
+                nx,ny = conv[i]
+                if abs(nx - x) + abs(ny - y) <= 1000:
+                    queue.append([nx,ny])
+                    visited[i] = True
+    print("sad")
+
 t = int(sys.stdin.readline())
 for _ in range(t):
     n = int(sys.stdin.readline())
-    beer = 1000
-    x,y = map(int,sys.stdin.readline().split()) # 시작
-    check = 0
+    # 시작위치 + 편의점 위치
+    conv = []
     for _ in range(n+1):
-        tmpx,tmpy = map(int,sys.stdin.readline().split())
-        distance = math.sqrt((tmpy-y)**2+(tmpx-x)**2)
-        x,y = tmpx,tmpy
-        if beer >= distance:
-            beer = 1000
-        else:
-            check += 1
-            break
-    if check != 0:
-        print("sad")
-    else:
-        print("happy")
+        conv.append(list(map(int,sys.stdin.readline().split())))
+    # 도착 위치
+    ex,ey = map(int,sys.stdin.readline().split())
+    visited = [False]*(n+1)
+    bfs()
+
 
