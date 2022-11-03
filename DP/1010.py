0e5762c797946554a25de9dbd82e496d1b99177d
yerimstar <yerimhuh@naver.com>
2022-11-03 17:03:55 +0900

    [221102] 완료

--- DP/1010.py
+++ DP/1010.py
@@ -1,7 +1,16 @@
 # 다리 놓기
 import sys
-from itertools import combinations
 t = int(sys.stdin.readline())
 for _ in range(t):
     n,m = map(int,sys.stdin.readline().split())
-    print(len(list(combinations(list(range(m)),n))))
+    dp = [[0 for _ in range(m+1)] for _ in range(m+1)]
+    for i in range(m+1):
+        for j in range(m+1):
+            if j == 0:
+                dp[i][j] = 1
+            else:
+                if i == j:
+                    dp[i][j] = 1
+                else:
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+    print(dp[m][n])
