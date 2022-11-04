d646ebb3b3eddb9dcd7c3038b308a891b4a5319e
yerimstar <yerimhuh@naver.com>
2022-11-04 12:11:04 +0900

    [221104] 완료

--- BFS,DFS/1926.py
+++ BFS,DFS/1926.py
@@ -1,32 +1,41 @@
 # 그림
 import sys
-sys.setrecursionlimit(10**6)
-def dfs(x,y):
+from collections import deque
+
+def bfs(x,y):
     global cnt
-    move = [[0,1],[0,-1],[1,0],[-1,0]]
-    if x <= -1 or x >= n or y <= -1 or y >= m:
-        return False
-    if graph[x][y] == 1:
-        graph[x][y] = 0
+    q = deque()
+    q.append([x,y])
+    visited[x][y] = True
+
+    while q:
+        x,y = q.popleft()
         cnt += 1
-        for mx, my in move:
+        for mx,my in move:
             dx = x + mx
             dy = y + my
-            dfs(dx, dy)
-        return True
-    return False
+            # 도화지 범위 내에 있는 지점인지 확인
+            if 0 <= dx < n and 0 <= dy < m:
+                # 색칠되어 있고 아직 방문하지 않은 지점
+                if graph[dx][dy] and not visited[dx][dy]:
+                    graph[dx][dy] = 0
+                    visited[dx][dy] = True
+                    q.append([dx, dy])
+    return cnt
 
 n,m = map(int,sys.stdin.readline().split())
 graph = []
+move = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+visited = [[False for _ in range(m)] for _ in range(n)]
 for _ in range(n):
     graph.append(list(map(int,sys.stdin.readline().split())))
 
 tmp = []
 for i in range(n):
     for j in range(m):
-        cnt = 0
-        if dfs(i,j):
-            tmp.append(cnt)
+        if graph[i][j] == 1 and not visited[i][j]:
+            cnt = 0
+            tmp.append(bfs(i,j))
 
 if len(tmp) == 0:
     print(0)
