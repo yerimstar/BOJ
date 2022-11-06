c1f39f3f92d7f3681c8a189a5fbdf43548aecd5e
yerimstar <yerimhuh@naver.com>
2022-11-06 15:25:36 +0900

    [221106] 주석추가

--- BFS,DFS/1303.py
+++ BFS,DFS/1303.py
@@ -9,9 +9,11 @@ def bfs(x,y,check):
     visited[x][y] = True
     while q:
         x,y = q.popleft()
+        # 이동 방향
         for mx,my in move:
             dx = x + mx
             dy = y + my
+            # 전쟁터 밖 확인 
             if 0 <= dx < m and 0 <= dy < n:
                 if check == 'W' and not visited[dx][dy] and graph[dx][dy] == 'W':
                     cnt_w += 1
@@ -26,18 +28,21 @@ def bfs(x,y,check):
     elif check == 'B':
         return cnt_b**2
 
-n,m = map(int,sys.stdin.readline().split())
-graph = []
-visited = [[False for _ in range(n)] for _ in range(m)]
-move = [[0,1],[0,-1],[1,0],[-1,0]]
+n,m = map(int,sys.stdin.readline().split()) # 가로, 세로 길이
+graph = [] # 전쟁터
+visited = [[False for _ in range(n)] for _ in range(m)] # 방문 처리
+move = [[0,1],[0,-1],[1,0],[-1,0]] # 이동 방향
+# 전쟁터 병사 위치 입력 받기
 for _ in range(m):
     graph.append(list(sys.stdin.readline().strip()))
 white = []
 blue = []
 for i in range(m):
     for j in range(n):
+        # 우리팀(흰색) 병력 체크
         if graph[i][j] == 'W' and not visited[i][j]:
             white.append(bfs(i,j,'W'))
+        # 상대팀(파란색) 병력 체크
         elif graph[i][j] == 'B' and not visited[i][j]:
             blue.append(bfs(i,j,'B'))
 
