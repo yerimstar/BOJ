71e2c6f63046748b0cf2192956d7ceef0407579f
yerimstar <yerimhuh@naver.com>
2022-11-09 21:15:55 +0900

    [221109] 완료

--- BFS,DFS/9019.py
+++ BFS,DFS/9019.py
@@ -5,9 +5,14 @@ from collections import deque
 def bfs(s,e):
     q = deque()
     q.append(s)
+    visited[s] = 'Z'
     while q:
         n = q.popleft()
         for m in move:
+            d1 = n // 1000
+            d2 = (n-1000*d1) // 100
+            d3 = (n-1000*d1-100*d2) // 10
+            d4 = n-1000*d1-100*d2-10*d3
             if m == 'D':
                 if 2 * n > 9999:
                     register = 2 * n % 10000
@@ -19,24 +24,20 @@ def bfs(s,e):
                 else:
                     register = n - 1
             elif m == 'L':
-                if len(str(n)) == 4:
-                    n = str(n)
-                    register = int(n[1] + n[2] + n[3] + n[0])
+                register = int(str(d2) + str(d3) + str(d4) + str(d1))
             elif m == 'R':
-                if len(str(n)) == 4:
-                    n = str(n)
-                    register = int(n[3] + n[0] + n[1] + n[2])
+                register = int(str(d4) + str(d1) + str(d2) + str(d3))
             n = int(n)
-            if 0 <= register < 10000:
+            if 0 <= register < 10000 and visited[register] == '':
                 q.append(register)
                 visited[register] = visited[n] + m
                 if register == e:
                     return
-    return
+
 t = int(sys.stdin.readline())
 move = ['D','S','L','R']
 for _ in range(t):
     visited = ['' for _ in range(10000)]
     A,B = map(int,sys.stdin.readline().split())
     bfs(A,B)
-    print(visited[B])
+    print(visited[B][1:])
