8afecb6c8e9de4ec67ef8d7666c51675ff5eb326
yerimstar <yerimhuh@naver.com>
2022-11-13 21:53:36 +0900

    [221113] 완료

--- etc/10845.py
+++ etc/10845.py
@@ -1,4 +1,38 @@
-from collections import deque
 import sys
+from collections import deque
+
 N = int(sys.stdin.readline())
-print(N)
+q = deque()
+for _ in range(N):
+    # print("q = ",q)
+    tmp = sys.stdin.readline().split()
+    if len(tmp) == 2:
+        s = tmp[0]
+        num = int(tmp[1])
+    elif len(tmp) == 1:
+        s = tmp[0]
+
+    if s == 'push':
+        q.append(num)
+    elif s == 'pop':
+        if q:
+            print(q.popleft())
+        else:
+            print("-1")
+    elif s == 'size':
+        print(len(q))
+    elif s == 'empty':
+        if q:
+            print(0)
+        else:
+            print(1)
+    elif s == 'front':
+        if q:
+            print(q[0])
+        else:
+            print("-1")
+    elif s == 'back':
+        if q:
+            print(q[-1])
+        else:
+            print("-1")
