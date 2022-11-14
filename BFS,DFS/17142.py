5e53201fe1bd64b723c62726cdbf3c9b67c3ddc5
yerimstar <yerimhuh@naver.com>
2022-11-15 00:54:21 +0900

    [221113] 완료

--- BFS,DFS/17142.py
+++ BFS,DFS/17142.py
@@ -7,31 +7,33 @@ def bfs(v):
     visited = [[0 for _ in range(n)] for _ in range(n)]
     q = deque()
     tmp_graph = [x[:] for x in graph]
-    for x,y in v:
-        q.append([x,y])
+
+    for x,y,t in v:
+        q.append([x,y,t])
         visited[x][y] = 1
-        tmp_graph[x][y] = 0
+
+    cnt = 0
     while q:
-        x,y = q.popleft()
+        x,y,c = q.popleft()
         for mx,my in move:
             dx = x + mx
             dy = y + my
             if 0 <= dx < n and 0 <= dy < n:
-                if not visited[dx][dy] and tmp_graph[dx][dy] == 0:
+                if not visited[dx][dy] and tmp_graph[dx][dy] != 1:
                     visited[dx][dy] = 1
-                    tmp_graph[dx][dy] = tmp_graph[x][y] + 1
-                    q.append([dx,dy])
-    cnt = 0
+                    if tmp_graph[dx][dy] == 0:
+                        tmp_graph[dx][dy] = 2
+                        cnt = c + 1
+                    q.append([dx,dy,c + 1])
+    check = 0
     for i in range(n):
         for j in range(n):
-            if tmp_graph[i][j] == '*':
-                tmp_graph[i][j] = 0
-    if cnt == 0:
-        for i in range(n):
-            cnt = max(cnt,max(tmp_graph[i]))
-        return cnt
-    else:
+            if tmp_graph[i][j] == 0:
+                check += 1
+    if check > 0:
         return -1
+    else:
+        return cnt
 
 n,m = map(int,sys.stdin.readline().split())
 graph = []
@@ -42,13 +44,14 @@ for i in range(n):
     tmp = list(map(int,sys.stdin.readline().split()))
     for j in range(n):
         if tmp[j] == 2:
-            tmp[j] = '*'
-            virus.append([i,j])
+            virus.append([i,j,0])
         elif tmp[j] == 0:
             empty += 1
     graph.append(tmp)
+
 virus_lst = list(combinations(virus,m))
 result = 10**6
+
 if empty == 0:
     result = 0
 else:
@@ -56,7 +59,6 @@ else:
         cnt = bfs(v)
         if cnt != -1:
             result = min(result,cnt)
-    if result == 10**6:
-        result = -1
-
+if result == 10**6:
+    result = -1
 print(result)
