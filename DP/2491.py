2d9bd88d3a6f27c2cafecf6a7b62b6efe6bfb44c
yerimstar <yerimhuh@naver.com>
2022-11-20 21:10:08 +0900

    [221120] 완료

--- DP/2491.py
+++ DP/2491.py
@@ -2,26 +2,22 @@
 import sys
 n = int(sys.stdin.readline())
 lst = list(map(int,sys.stdin.readline().split()))
-increase_lst = []
-decrease_lst = []
+result = 1
 cnt = 1
 for i in range(1,n):
     if lst[i-1] <= lst[i]:
         cnt += 1
     else:
-        increase_lst.append(cnt)
         cnt = 1
-if cnt > 1:
-    increase_lst.append(cnt)
+    if result < cnt:
+        result = cnt
 
 cnt = 1
 for i in range(1,n):
     if lst[i-1] >= lst[i]:
         cnt += 1
     else:
-        decrease_lst.append(cnt)
         cnt = 1
-if cnt > 1:
-    decrease_lst.append(cnt)
-result = max(max(increase_lst),max(decrease_lst))
+    if result < cnt:
+        result = cnt
 print(result)
