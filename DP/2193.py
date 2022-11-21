03273af9d40681a0316fcbc270ef7fbeca6b49d4
yerimstar <yerimhuh@naver.com>
2022-11-21 17:54:48 +0900

    [221121] 완료

--- DP/2193.py
+++ DP/2193.py
@@ -11,5 +11,5 @@ dp[4] = [2,1,3]
 for i in range(5,n+1):
     dp[i][0] = dp[i-1][0]+dp[i-1][1]
     dp[i][1] = dp[i-1][0]
-    dp[i][2] = dp[i-1][0]*2+dp[i-1][1]
+    dp[i][2] = dp[i][0]+dp[i][1]
 print(dp[n][2])
