b06bd64b602f11328376b7a1f72cb06d1ffa8154
yerimstar <yerimhuh@naver.com>
2022-12-04 13:29:13 +0900

    [221204] 완료

--- DP/2225.py
+++ DP/2225.py
@@ -1,9 +1,9 @@
 # 합분해
 import sys
 n,k = map(int,sys.stdin.readline().split())
-dp = [[1 for _ in range(n+1)] for _ in range(k+1)]
+dp = [[1 for _ in range(n)] for _ in range(k)]
 
 for i in range(1,k):
-    for j in range(n+1):
+    for j in range(n):
         dp[i][j] = dp[i-1][j]+dp[i][j-1]
-print(dp[k-1][n-1])
+print(dp[k-1][n-1]%10**9)
