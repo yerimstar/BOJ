fda55e8a44e946addaa5bb9c63ccbe26a8ef8618
yerimstar <yerimhuh@naver.com>
2022-12-11 21:02:19 +0900

    [221211] 완료

--- DP/2133.py
+++ DP/2133.py
@@ -2,7 +2,8 @@
 import sys
 n = int(sys.stdin.readline())
 dp = [0 for _ in range(n+1)]
-dp[2] = 3
+if n >= 2:
+    dp[2] = 3
 for i in range(4,n+1,2):
     dp[i] = dp[i-2] * 3
     for j in range(0,i-2,2):
